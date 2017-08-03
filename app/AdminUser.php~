c743f66e7342f25d3a05be3adb60e33ad409b7ed
Waqas Raza <waqasraza123@gmail.com>
2017-08-03 10:09:12 +0500

    1-Implemented Admin Login 2-Protect all authenticated routes for Amdin except Dashboard 3-created Login view for admin or other users

---
+++ app/AdminUser.php~
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class AdminUser extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
--- database/migrations/2017_08_02_200104_create_admin_user_table.php
+++ database/migrations/2017_08_02_200104_create_admin_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateAdminUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admin_users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email', 50)->nullable();
-            $table->string('password')->nullable();
-            $table->string('provider');
-            $table->string('provider_id')->unique();
-            $table->string('type')->default('1');
+            $table->string('email', 50)->unique();
+            $table->string('password');
             $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
@@ -34,6 +31,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+    	Schema::dropIfExists('admin');
+        Schema::dropIfExists('admin_users');
     }
 }
