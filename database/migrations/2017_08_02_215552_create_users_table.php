eb2560924eaa5fd0eacd85667ebb43d987bbcacd
Chaudhry Waqas <waqasraza123@gmail.com>
2017-08-15 17:35:25 +0500

    fixed users table migration issue

--- database/migrations/2017_08_02_215552_create_users_table.php
+++ database/migrations/2017_08_02_215552_create_users_table.php
@@ -13,7 +13,7 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
 
             $table->increments('id');
             $table->string('name');
